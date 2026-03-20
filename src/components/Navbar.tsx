b4d5a9b6ca4c39eb2d65c106d61b916a8223896b
vyra <nathanwong1776@gmail.com>
2026-03-20 01:33:50 -0700

    landing page mockup finished in 08 minutes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,9 +12,9 @@ const NavBar: React.FC = () => {
   const role = userWithRole?.name;
   const pathName = usePathname();
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="/">Next.js Application Template</Navbar.Brand>
+        <Navbar.Brand href="/">digits</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto justify-content-start">
