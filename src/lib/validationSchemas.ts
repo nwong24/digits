b362f498dd633ddac976f8748d76d994a9ca6335
vyra <nathanwong1776@gmail.com>
2026-03-31 14:33:08 -1000

    contact forms finished in 04 minutes

--- src/lib/validationSchemas.ts
+++ src/lib/validationSchemas.ts
@@ -24,3 +24,22 @@ export const EditStuffSchema = Yup.object({
   condition: Yup.string().oneOf(['excellent', 'good', 'fair', 'poor']).required(),
   owner: Yup.string().required(),
 });
+
+export const AddContactSchema = Yup.object({
+  firstName: Yup.string().required(),
+  lastName: Yup.string().required(),
+  address: Yup.string().required(),
+  image: Yup.string().required(),
+  description: Yup.string().required(),
+  owner: Yup.string().required(),
+});
+
+export const EditContactSchema = Yup.object({
+  id: Yup.number().required(),
+  firstName: Yup.string().required(),
+  lastName: Yup.string().required(),
+  address: Yup.string().required(),
+  image: Yup.string().required(),
+  description: Yup.string().required(),
+  owner: Yup.string().required(),
+});
